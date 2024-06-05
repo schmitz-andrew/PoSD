763942e5e9b77737e5af3c3dea3f3b18833c397d
Nadine <34283749+nmulle@users.noreply.github.com>
2024-06-05 14:32:59 +0200

    add room plugin to export schemas

--- FoodTracker/app/build.gradle.kts
+++ FoodTracker/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.googleDevtoolsKsp)
+    alias(libs.plugins.androidxRoom)
 }
 
 android {
@@ -45,6 +46,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
+
 }
 
 dependencies {
--- FoodTracker/build.gradle.kts
+++ FoodTracker/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.googleDevtoolsKsp) apply false
+    alias(libs.plugins.androidxRoom) apply false
 }
