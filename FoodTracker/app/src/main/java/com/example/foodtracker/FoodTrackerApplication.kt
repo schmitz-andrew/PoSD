57eae8a88c713432bb9726e086537a33fa16168a
Nadine <34283749+nmulle@users.noreply.github.com>
2024-06-18 21:18:40 +0200

    allow reminders to be sent a week or a day before product expiry

--- FoodTracker/app/src/main/java/com/example/foodtracker/FoodTrackerApplication.kt
+++ FoodTracker/app/src/main/java/com/example/foodtracker/FoodTrackerApplication.kt
@@ -2,12 +2,17 @@ package com.example.foodtracker
 
 import android.app.Application
 import com.example.foodtracker.data.AppDatabase
+import com.example.foodtracker.data.WorkManagerReminderRepository
 
 
 class FoodTrackerApplication: Application() {
 
     val database by lazy {
         AppDatabase.getDatabase(this)
+    }
+
+    val reminderRepository by lazy {
+        WorkManagerReminderRepository(this)
     }
 
 }
